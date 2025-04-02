a164ed5a75be3792db5d749b37b504d596fdc735
jeonghoon-lge <153166311+jeonghoon-lge@users.noreply.github.com>
2025-04-02 16:41:58 +0900

    [PLOVER-210] add flutter package files (#3)
    * [PLOVER-210] add flutter package files
    
    * Update pubspec.yaml
    
    fix description and version

---
+++ lib/design_tokens.dart
@@ -0,0 +1,18 @@
+library design_tokens;
+
+class TokenPaths {
+  // core token file paths
+  static const String corePalette = 'packages/core-tokens/json/color-palette.json';
+
+  // webOS token file paths
+  static const String webosBrand =
+      'packages/webos-tokens/json/color-brand.json';
+  static const String webosAlias = 'packages/webos-tokens/json/alias.json';
+  static const String webosBorderRadius =
+      'packages/webos-tokens/json/border-radius.json';
+  static const String webosFontSize =
+      'packages/webos-tokens/json/font-size.json';
+  static const String webosFontWeight =
+      'packages/webos-tokens/json/font-weight.json';
+  static const String webosSpacing = 'packages/webos-tokens/json/spacing.json';
+}
