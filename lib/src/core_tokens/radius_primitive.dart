bdf0c7cfa8e5bd7505e424f4b5dfbadb74ee0398
jeonghoon.yi <jeonghoon.yi@lge.com>
2025-06-27 15:36:58 +0900

    standardize radius variable naming

--- lib/src/core_tokens/radius_primitive.dart
+++ lib/src/core_tokens/radius_primitive.dart
@@ -11,21 +11,21 @@ class RadiusPrimitive {
   static RadiusPrimitive? _instance;
   static RadiusPrimitive get instance => _instance ??= RadiusPrimitive._();
 
-  late final Radius radius_0 = Radius.zero;
-  late final Radius radius_2 = const Radius.circular(2);
-  late final Radius radius_4 = const Radius.circular(4);
-  late final Radius radius_6 = const Radius.circular(6);
-  late final Radius radius_8 = const Radius.circular(8);
-  late final Radius radius_10 = const Radius.circular(10);
-  late final Radius radius_12 = const Radius.circular(12);
-  late final Radius radius_16 = const Radius.circular(16);
-  late final Radius radius_20 = const Radius.circular(20);
-  late final Radius radius_24 = const Radius.circular(24);
-  late final Radius radius_30 = const Radius.circular(30);
-  late final Radius radius_36 = const Radius.circular(36);
-  late final Radius radius_40 = const Radius.circular(40);
-  late final Radius radius_44 = const Radius.circular(44);
-  late final Radius radius_48 = const Radius.circular(48);
-  late final Radius radius_60 = const Radius.circular(60);
-  late final Radius radius_999 = const Radius.circular(999);
+  late final Radius radius0 = Radius.zero;
+  late final Radius radius2 = const Radius.circular(2);
+  late final Radius radius4 = const Radius.circular(4);
+  late final Radius radius6 = const Radius.circular(6);
+  late final Radius radius8 = const Radius.circular(8);
+  late final Radius radius10 = const Radius.circular(10);
+  late final Radius radius12 = const Radius.circular(12);
+  late final Radius radius16 = const Radius.circular(16);
+  late final Radius radius20 = const Radius.circular(20);
+  late final Radius radius24 = const Radius.circular(24);
+  late final Radius radius30 = const Radius.circular(30);
+  late final Radius radius36 = const Radius.circular(36);
+  late final Radius radius40 = const Radius.circular(40);
+  late final Radius radius44 = const Radius.circular(44);
+  late final Radius radius48 = const Radius.circular(48);
+  late final Radius radius60 = const Radius.circular(60);
+  late final Radius radius999 = const Radius.circular(999);
 }
--- lib/src/webos_tokens/radius_semantic.dart
+++ lib/src/webos_tokens/radius_semantic.dart
@@ -10,9 +10,9 @@ import '../core_tokens/radius_primitive.dart';
 class RadiusSemantic {
   const RadiusSemantic();
 
-  Radius get full => RadiusPrimitive.instance.radius_999;
-  Radius get button => RadiusPrimitive.instance.radius_24;
-  Radius get container => RadiusPrimitive.instance.radius_36;
-  Radius get overlay => RadiusPrimitive.instance.radius_60;
-  Radius get popup => RadiusPrimitive.instance.radius_60;
+  Radius get full => RadiusPrimitive.instance.radius999;
+  Radius get button => RadiusPrimitive.instance.radius24;
+  Radius get container => RadiusPrimitive.instance.radius36;
+  Radius get overlay => RadiusPrimitive.instance.radius60;
+  Radius get popup => RadiusPrimitive.instance.radius60;
 }
